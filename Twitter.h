581ef8af0b46af5062cb6a35e9cdee5a93021de4
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2025-03-17 23:39:18 -0400

    Create Twitter.h

---
+++ Twitter.h
@@ -0,0 +1,42 @@
+// Twitter.h
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "Profile.h"  // Include the Profile struct definition
+
+template <typename T>
+class Twitter {
+private:
+    T followedUser;
+    std::vector<T> followers;
+    const size_t maxFollowers = 5;
+
+public:
+    Twitter(T user) : followedUser(user) {}
+
+    void addFollower(T follower) {
+        if (followers.size() < maxFollowers) {
+            followers.push_back(follower);
+        } else {
+            std::cout << "Follower list is full!" << std::endl;
+        }
+    }
+
+    void removeFollower(T follower) {
+        auto it = std::remove(followers.begin(), followers.end(), follower);
+        if (it != followers.end()) {
+            followers.erase(it, followers.end());
+        } else {
+            std::cout << "Follower not found!" << std::endl;
+        }
+        printFollowers();
+    }
+
+    void printFollowers() const {
+        std::cout << "Followers of " << followedUser << ":" << std::endl;
+        for (const auto& follower : followers) {
+            std::cout << follower << std::endl;
+        }
+    }
+};
